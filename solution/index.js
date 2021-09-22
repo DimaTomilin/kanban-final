a20a520c2a086317cdcb8528dd658df71de5e41e
holdbracha <0527161548a@gmail.com>
2021-09-22 16:38:54 +0300

    Added event to button submit
    Added event that uses function
    The function adds "li" element to list, add the same task in localStorage Object and also updated localStorage in the browser.
    Also added some CSS styles to more comfortable work

--- solution/index.js
+++ solution/index.js
@@ -18,9 +18,40 @@ function createElement(tagName, children = [], classes = [], attributes = {}, ev
     }
     return  el
 }
+
+function addTaskClickEvent(event) {
+    const target=event.target
+    if(target.tagName!=="BUTTON"){
+        return;
+    }
+    const input=target.previousElementSibling
+    let inputValue=input.value
+    if(inputValue===""){
+        alert ("You can`t add empty task.")
+        return;
+    }
+    const listElement=target.parentElement.querySelector("ul")
+    const listItemElement=createElement("li", [inputValue],["list-item"])
+    listElement.append(listItemElement)
+    switch(target.id){
+        case "submit-add-to-do":
+            localStorageObject.todo.unshift(inputValue)
+            break;
+        case "submit-add-in-progress":
+            localStorageObject["in-progress"].unshift(inputValue)
+            break;
+        case "submit-add-done":
+            localStorageObject.done.unshift(inputValue)
+            break;
+    }
+    localStorage.tasks=JSON.stringify(localStorageObject);
+    input.value="";
+}
+
 localStorage.setItem("tasks","");
 const localStorageObject={
     "todo":[],
     "in-progress":[],
     "done":[],
 }
+document.querySelector("main").addEventListener("click", addTaskClickEvent)
