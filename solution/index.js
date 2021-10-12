e4a48ff8183086b2b28ced013a3767b5d12e5333
DimaTomilin <89575657+DimaTomilin@users.noreply.github.com>
2021-10-12 13:07:07 +0300

    Optimization of all code
    I maked functions shorter

--- solution/index.js
+++ solution/index.js
@@ -41,7 +41,7 @@ function generationTasklist(){
     const allTasks = getTasksFromStorage()
 
     for(const [taskType, taskList] of Object.entries(allTasks)){
-        const taskListElement = document.querySelector(`[data-type=${taskType}]`).closest("section").querySelector("ul");
+        const taskListElement = document.querySelector(`[data-type=${taskType}]`).querySelector("ul");
         
         taskList.forEach(function(task) {
             taskListElement.appendChild(createNewTaskElement(task));
@@ -63,7 +63,7 @@ function deletingAllTasks(){
 function refreshTaskSection(){
     deletingAllTasks();
     generationTasklist()
-    generationImportantTasksFromLocalStorage(importantTasksArray)
+    generationImportantTasks()
 }
 
 
@@ -75,6 +75,64 @@ Network
 *
 *
 */
+
+const TASKS_API_URI = 'https://json-bins.herokuapp.com/bin/6162f1049e72744bcfc40980';
+
+async function request(method = '', data = null) {
+    const options = {
+      method: method,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    };
+  
+    if (data) {
+      options.body = JSON.stringify(data);
+    }
+
+    const response = await fetch(TASKS_API_URI, options);
+    errorCatch(response);
+    return response.json();
+}
+  
+async function loadTasksFromApi() {
+    const loaded = await request('GET', null);
+
+    return loaded.tasks;
+}
+  
+async function saveTasksToApi(tasks) {
+    return request('PUT', {tasks});
+}
+
+function errorCatch(response){
+    if(!response.ok){ // error part: behavior of all elements in error situation anf throw ERROR with current status
+        alert("ERROR "+response.status+". "+response.statusText)//displaying in result element at error message
+        throw `ERROR ${response.status}`
+    }
+}
+
+//Fucntion of save tasks in API Storage
+async function saveToStorage(){
+    displayLoading();
+
+    const tasks = getTasksFromStorage()
+    await saveTasksToApi(tasks)
+   
+    hideLoading();
+}
+
+////Fucntion of loading tasks from API Storage if in localStorage we have other tasks function refreshes localStorage information to API information 
+async function loadFromStorage(){
+    displayLoading();
+
+    const tasks = await loadTasksFromApi()
+    updateTasksInLocalStorage(tasks);
+    refreshTaskSection();
+
+    hideLoading();
+}
 
 
 
@@ -148,12 +206,12 @@ function validateTask(taskText) {
 
 //Displaying of loader element
 function displayLoading(){
-    loaderElement.classList.add("loader")
+    document.querySelector("#loader").classList.add("loader")
 }
 
 //Stop displaying of loading element
 function hideLoading(){
-    loaderElement.classList.remove("loader")
+    document.querySelector("#loader").classList.remove("loader")
 }
 
 //Function that make task text editable and after unfocus saves changes
@@ -165,9 +223,9 @@ function dblclickEditTaskEvent(event){
 }
 
 //Function of event listener that addes new task to target list
-function addTaskClickEvent(event) {
+function addTask(event) {
     const input = event.target.closest("section").querySelector("input")
-    const taskType = event.target.dataset.type;
+    const taskType = event.target.closest("section").dataset.type;
 
     updateTasks(input.value, taskType);
     input.value = "";
@@ -189,8 +247,7 @@ function updateTasks(task, taskType){
 
    currentTasks[taskType].unshift(validateTask(task));
    updateTasksInLocalStorage(currentTasks);
-
-   refreshTaskSection(currentTasks);
+   refreshTaskSection();
 }
  
 function makingEditable(element){
@@ -229,133 +286,91 @@ function changingTask(task, newTask){
 
 
 //Removing of replaced task element from old localStorage section
-function removeReplacingElement(elementValue){
-    for(const section in localStorageObject){
-        let propertyArray = localStorageObject[section]
-        if(propertyArray.includes(elementValue)){
-            const indexOfElement = propertyArray.indexOf(elementValue);
-            propertyArray.splice(indexOfElement, 1)
+function removeReplacingElement(task){
+    const tasks = getTasksFromStorage();
+
+    for(const [taskType, taskList] of Object.entries(tasks)){ 
+        if(taskList.includes(task)){
+            taskList.splice(taskList.indexOf(task), 1)
         }
     }
+    updateTasksInLocalStorage(tasks)
 }
+
+function checkedKeyDown(event){
+    const key = parseInt(event.key)
+            if(key > 3 || event.altKey === false || !event.code.includes("Digit")){ //Check that key is exist
+                throw Error ;
+            } else {return key}
+    // if(!event.code.includes("Digit") && event.altKey === false){//Check if key is number and check altKEy
+    //     return;
+    // }
+    
+}
+
+function chooseTaskType(key){
+    let taskType;
+    switch(key){
+        case 1:
+            taskType = "todo"
+            break;
+        case 2:
+            taskType = "in-progress"
+            break;
+        case 3:
+            taskType = "done"
+            break;
+    }
+    return taskType;
+}
+
+function replacingFunction(task, taskType){
+    removeReplacingElement(task)
+    updateTasks(task, taskType)
+}
+
+function addReplacingFunction(task){
+    window.onkeydown = function (event) {
+        const key = parseInt(event.key)
+        if(key > 3 || event.altKey === false || !event.code.includes("Digit")){ //Check that key is exist
+           return ;
+        }
+        const taskType = chooseTaskType(key)
+        replacingFunction(task, taskType)
+        window.onkeydown = null;
+    }
+}
+
 
 //Using of Alt+1,2,3 to replace tast between sections
 function replaceOfTask (event) {
     const targetElement = event.target.closest("li")
-    window.onkeydown = function (event2) {
-        if(!event2.code.includes("Digit")){//Check if key is number
-            return
-        }
-        if(event2.altKey === false){ //Check altKEy
-            return
-        }
-        const key = parseInt(event2.key)
-        if(key>3){ //Check that key is exist
-            return
-        }
-
-        let newParentElement; //PLace where I want to replace the task
-        let newLocalStorageArray; //Updating localStorage
-        switch(key){
-            case 1:
-                newParentElement = document.querySelector(".to-do-tasks")
-                newLocalStorageArray = localStorageObject.todo
-                break;
-            case 2:
-                newParentElement = document.querySelector(".in-progress-tasks")
-                newLocalStorageArray = localStorageObject["in-progress"]
-                break;
-            case 3:
-                newParentElement = document.querySelector(".done-tasks")
-                newLocalStorageArray = localStorageObject.done
-                break;
-        }
-        newParentElement.insertBefore(targetElement, newParentElement.childNodes[0]); //Added new task to the top of list
-        removeReplacingElement(targetElement.innerText)
-        newLocalStorageArray.unshift(targetElement.innerText)
-        localStorage.tasks = JSON.stringify(localStorageObject)
-        window.onkeydown = null;
-    };
+    addReplacingFunction(targetElement.textContent)
 }
 
-
-
-//Fucntion of search input that do refresh of all tasks and sort to you all tasks that include text from input area
-function searchTasks(){
-    const valueInInput = document.getElementById("search").value.toLowerCase()
-    refreshTaskSection();
+function deletingElementsIncludes(text){
     const allTasks = document.querySelectorAll(".task")
     for(let task of allTasks){
         const taskValue = task.innerText.toLowerCase()
-        if(!taskValue.includes(valueInInput)){
+        if(!taskValue.includes(text)){
             task.parentElement.removeChild(task)
         }
     }
 }
 
 
-const TASKS_API_URI = 'https://json-bins.herokuapp.com/bin/6162f1049e72744bcfc40980';
-
-async function request(method = '', data = null) {
-    const options = {
-      method: method,
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-    };
-  
-    if (data) {
-      options.body = JSON.stringify(data);
-    }
-
-    const response = await fetch(TASKS_API_URI, options);
-    errorCatch(response);
-    return response.json();
-}
-  
-async function loadTasksFromApi() {
-    const loaded = await request('GET', null);
-
-    return loaded.tasks;
-}
-  
-async function saveTasksToApi(tasks) {
-    return request('PUT', {tasks});
-}
-
-function errorCatch(response){
-    if(!response.ok){ // error part: behavior of all elements in error situation anf throw ERROR with current status
-        alert("ERROR "+response.status+". "+response.statusText)//displaying in result element at error message
-        throw `ERROR ${response.status}`
-    }
-}
-
-//Fucntion of save tasks in API Storage
-async function saveToStorage(){
-    displayLoading();
-
-    const tasks = getTasksFromStorage()
-    await saveTasksToApi(tasks)
-   
-    hideLoading();
-}
-
-////Fucntion of loading tasks from API Storage if in localStorage we have other tasks function refreshes localStorage information to API information 
-async function loadFromStorage(){
-    displayLoading();
-
-    const tasks = await loadTasksFromApi()
-    updateTasksInLocalStorage(tasks);
+//Fucntion of search input that do refresh of all tasks and sort to you all tasks that include text from input area
+function searchTasks(){
+    const valueInInput = document.getElementById("search").value.toLowerCase()
     refreshTaskSection();
-
-    hideLoading();
+    deletingElementsIncludes(valueInInput)
 }
+
+
 
 //Starting drag event
 function dragEvent (event){
-    draggingElement = event.target.closest("li")
-    event.dataTransfer.setData("text", event.target.closest("ul").className);
+    draggingTask = event.target.closest("li").textContent
 
     const sections = document.querySelectorAll("section") //Adding event listener to all sections
     for(let section of sections){
@@ -369,69 +384,64 @@ function allowDrop(event) {
     event.preventDefault();
 }
 
+function deletingImportantTask(task){
+    const importantTasks = getImportantTasksFromStorage();
+
+    if(importantTasks.includes(task)){ //Deleting from importantTasksArray deleted tasks
+        importantTasks.splice(importantTasks.indexOf(task), 1)
+        updateImportantTasksInLocalStorage(importantTasks)
+    }
+}
+
+//Check if I want to delete or replace task
+function choosingDropEvent(event, newSection){
+    switch(newSection){ 
+        case "dropzone":
+            const taskType = event.target.closest("section").dataset.type;
+            replacingFunction(draggingTask, taskType)
+            break;
+        case "removezone":
+            removeReplacingElement(draggingTask);
+            deletingImportantTask(draggingTask)
+            refreshTaskSection()
+            break;
+    }
+}
 //Finish of dragging, drop event
 function dropEvent(event) {
     event.preventDefault();
 
-    const previousParentClassName = event.dataTransfer.getData("text");
     const newSectionClass = event.target.closest("section").className
-    const newParentList = event.target.closest("section").querySelector("ul")
-    const valueOfDraggingElement = draggingElement.textContent;
-
-    switch(newSectionClass){ //Check if I want to delete or replace task
-        case "dropzone":
-            newParentList.insertBefore(draggingElement, newParentList.childNodes[0]);
-            removeReplacingElement(valueOfDraggingElement);
-            let localStorageArray;
-            switch(newParentList.className){
-                case "in-progress-tasks":
-                    localStorageArray = localStorageObject["in-progress"]
-                    break;
-                case "to-do-tasks":
-                    localStorageArray = localStorageObject["todo"]
-                    break;
-                case "done-tasks":
-                    localStorageArray = localStorageObject["done"]
-                    break;
-            }
-            localStorageArray.unshift(valueOfDraggingElement)
-            localStorage.tasks = JSON.stringify(localStorageObject)
-            break;
-        case "removezone":
-            document.querySelector(`.${previousParentClassName}`).removeChild(draggingElement);
-            removeReplacingElement(valueOfDraggingElement);
-
-            if(importantTasksArray.includes(valueOfDraggingElement)){ //Deleting from importantTasksArray deleted tasks
-                importantTasksArray.splice(importantTasksArray.indexOf(valueOfDraggingElement), 1)
-                localStorage.important = JSON.stringify(importantTasksArray)
-            }
-
-            localStorage.tasks = JSON.stringify(localStorageObject)
-            break;
-    }
+    choosingDropEvent(event, newSectionClass)
+    
 }
 
+ function addingtoImportantTasks(task){
+    const importantTasks = getImportantTasksFromStorage()
+
+    importantTasks.unshift(task)
+    updateImportantTasksInLocalStorage(importantTasks)
+ }
 //Event listener of click on important icon that use toggle(to add or to remove indication)
 function toggleToImportantTasks(event){
-    const target = event.target;
-    const task = target.closest("li").firstChild.innerText
-    if(target.closest("li").className.includes("important")){
-        target.closest("li").classList.remove("important")
-        importantTasksArray.splice(importantTasksArray.indexOf(task), 1);
-        localStorage.important = JSON.stringify(importantTasksArray)
+    const task = event.target.closest("li").firstChild.innerText
+    if(event.target.closest("li").className.includes("important")){
+        event.target.closest("li").classList.remove("important")
+        deletingImportantTask(task)
     } else {
-        target.closest("li").classList.add("important")
-        importantTasksArray.unshift(task)
-        localStorage.important = JSON.stringify(importantTasksArray)
+        addingtoImportantTasks(task)
+        generationImportantTasks()
     }
     
 }
 
 //Function that check all existing tasks if them important tasks
-function generationImportantTasksFromLocalStorage(importantTasksArray){
+function generationImportantTasks(){
+    const importantTasks = getImportantTasksFromStorage()
     const allTasksElements = document.querySelectorAll("li")
+
     for(const task of allTasksElements){
-        for(const importantTask of importantTasksArray){
+        for(const importantTask of importantTasks){
             if(task.firstChild.innerText === importantTask){
                 task.classList.add("important")
             }
@@ -440,37 +450,49 @@ function generationImportantTasksFromLocalStorage(importantTasksArray){
 }
 
 
-let draggingElement;
+let draggingTask;
 
-const loaderElement = document.querySelector("#loader")
+function getTasks() {
+    if(!getTasksFromStorage()){
+        updateTasksInLocalStorage({
+            todo: [],
+            'in-progress': [],
+            done: [],
+        });
+    } else { return getTasksFromStorage()}
+}
 
-let localStorageObject = {
-    "todo":[],
-    "in-progress":[],
-    "done":[],
+function getImportantTasks(){
+    if(!getImportantTasksFromStorage()){
+        updateImportantTasksInLocalStorage([]);
+    } else { return getImportantTasksFromStorage()}
+}
+
+function openingPage(){
+    getTasks();
+    getImportantTasks()
+    refreshTaskSection();
 }
 
 
+// if(localStorage.getItem("tasks") === null){
+//     localStorage.setItem("important","")
+//     localStorage.setItem("tasks","");
+//     localStorage.important = JSON.stringify(importantTasks)
+//     localStorage.tasks = JSON.stringify(localStorageObject);
+// } else {
+//     importantTasks = JSON.parse(localStorage.getItem("important"));
+//     localStorageObject = JSON.parse(localStorage.getItem("tasks"));
+//     generationTasklist();
+//     generationImportantTasks(importantTasks)
+// }
 
-let importantTasksArray = [];
-
-if(localStorage.getItem("tasks") === null){
-    localStorage.setItem("important","")
-    localStorage.setItem("tasks","");
-    localStorage.important = JSON.stringify(importantTasksArray)
-    localStorage.tasks = JSON.stringify(localStorageObject);
-} else {
-    importantTasksArray = JSON.parse(localStorage.getItem("important"));
-    localStorageObject = JSON.parse(localStorage.getItem("tasks"));
-    generationTasklist();
-    generationImportantTasksFromLocalStorage(importantTasksArray)
-}
+openingPage();
 
 const addButtons=document.querySelectorAll(".add-button")
 for(const button of addButtons){
-    button.addEventListener("click", addTaskClickEvent)
+    button.addEventListener("click", addTask)
 }
 document.getElementById("search").addEventListener("input", searchTasks)
 document.getElementById("save-btn").addEventListener("click", saveToStorage)
 document.getElementById("load-btn").addEventListener("click", loadFromStorage)
-
